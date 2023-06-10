c80249a64398b3841a1dfe3bc80e34cd5ac155ab
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-10 19:22:15 +0300

    большее и меньшее число

---
+++ Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Программа, которая на вход принимает два числа и выдает, какое число больше, а какое меньше
+a = 5 b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.Write("Введите число а: ");
+int numA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b: ");
+int numB = int.Parse(Console.ReadLine()!);
+if (numA == numB)
+    Console.WriteLine("Числа равны");
+else if (numA > numB)
+    Console.WriteLine($"Максимум {numA}, минимум {numB}");
+else
+    Console.WriteLine($"Максимум {numB}, минимум {numA}");
