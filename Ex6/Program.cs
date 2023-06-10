c714cc063eb6ad41137ff2011a6e8af8493c07cf
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-10 19:23:12 +0300

    четное или нет

---
+++ Ex6/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Программа, которая на вход принимает число и выдает, является ли оно четным
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+bool check = num % 2 == 0;
+Console.WriteLine(check);
