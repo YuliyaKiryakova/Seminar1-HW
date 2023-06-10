1c306ae6ca4a64d83e84886a632d232118f6c8a3
YuliyaKiryakova <yulk_krya@mail.ru>
2023-06-10 19:23:46 +0300

    все четные из интервала

---
+++ Ex8/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Программа, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N
+5 -> 2,4
+8 -> 2,4,6,8
+*/
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int start = 1;
+while (start <= num)
+{
+    if (start % 2 == 0)
+        Console.Write(start + ", ");
+    start++;
+}
